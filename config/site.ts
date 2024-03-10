11359acba48abb756a448ecb065ed83513f40b78
Tonya Christopher <christophert@teksavee.com>
2024-03-10 16:18:50 -0400

    header

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const siteConfig = {
+  name: "Teklez Blog",
+  url: "https://example.com",
+  description: "Lesbian Bool Blog",
+  author: "Teksavee",
+  links: {
+    twitter: "https://twitter.com/home",
+    github: "https://github.com/",
+    personalSite: ""
+  }
+}
+
+export type SiteConfig = typeof siteConfig
