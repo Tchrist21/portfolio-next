038f42b526151641d1df73282b0673004ea7f1c4
Tonya Christopher <christophert@teksavee.com>
2024-03-10 17:46:07 -0400

    setup dark mode

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils'
 import { Icons } from "./icons"
 import { MainNav } from "./main-nav"
 import { MobileNav } from "./mobile-nav"
+import { ModeToggle } from "./mode-toggle"
 
 export function SiteHeader() {
  return (
@@ -23,6 +24,7 @@ export function SiteHeader() {
             <span className="sr-only">Twitter</span>
           </div>
           </Link>
+          <ModeToggle/>
           <MobileNav/>
         </nav>
 
