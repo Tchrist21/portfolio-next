cfd60fbab8a9e5d723faad522494768d560485cd
Tonya Christopher <christophert@teksavee.com>
2024-03-15 18:18:21 -0400

    adding links in markdown

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -4,7 +4,7 @@ import { Icons } from './icons';
 export function SiteFooter() {
   return (
     <div className="mb-6 mt-14 flex flex-col items-center">
-      <div className="mb-3 flex space-x-4">
+      <div className="mb-3 flex gap-6 space-x-4">
         <a
           target="_blank"
           rel="noreffer"
@@ -26,7 +26,7 @@ export function SiteFooter() {
           <Icons.gitHub className="h-6 w-6" />
         </a>
       </div>
-      <div className="mb-2 space-x-2 text-sm text-muted-foreground">
+      <div className="mb-4 mt-2 space-x-2 text-sm text-muted-foreground">
         <a href={siteConfig.links.personalSite} target='_blank'>{siteConfig.author}</a>
 
       </div>
